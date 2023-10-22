89055f9f0482febc73602e460ac8ef239362cde2
Caitlin Kunchur <crkunchur@gmail.com>
2023-10-22 11:29:59 -0700

    reformat config files

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+       browser: true,
+       es2021: true,
+       jest: true
+    },
+    extends: ['plugin:react/recommended', 'airbnb', 'plugin:react/jsx-runtime', 'prettier'],
+    overrides: [],
+    parserOptions: {
+       ecmaVersion: 'latest',
+       sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+       'indent': 'off',
+       'import/order': 'off',
+       'react/jsx-filename-extension': 'off',
+       'camelcase': 'off',
+       'no-console': 'off',
+       'react/prop-types': 'off',
+       'no-nested-ternary': 'off',
+       'import/no-named-as-default': 'off',
+       'import/no-extraneous-dependencies': 'off',
+       ' import/no-useless-path-segments': 'off'
+    },
+ };
+ 
